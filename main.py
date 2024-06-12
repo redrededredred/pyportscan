f781a12b3ff0a05bbdb8a55df68745aecc1db40b
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2024-06-12 16:09:02 +0200

    Update main.py
    cli stuff

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 """
 CLI for pyportscan
 """
+import scanner
 
+def cli():
+    print("*** Pyportscan ***")
+    while True:
+        print()
+        address: str = input("Enter IPv4 or IPv4 Range to scan (e.g. 8.8.8.8 or 1.1.1.1/24): ")
+        port: int = int(input("Enter Port to scan (e.g. 80 or 21): "))
+        if "/" in address:
+            print(str(scanner.check_multiple_connections(address, port)))
+        else:
+            if scanner.check_connection(address, port):
+                print(f"Port {port} open on {address}")
+            else:
+                print("No connection possible")
 def main() -> None:
-    pass
+    cli()
 
 if __name__ == "__main__":
     main()
