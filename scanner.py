20dc357aa45a8d4a88f9b768ca442cc96cd526f3
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2024-06-12 15:10:12 +0200

    Update scanner

--- scanner.py
+++ scanner.py
@@ -1,22 +1,57 @@
 """
 Implements tcp portscanning capabilities.
 """
+import socket
+import ipaddress
 
-def handle_ipv4(address: str) -> list:
+def check_multiple_connections(address: str, port: int) -> list:
     """
-    Takes either a single IPv4 or a range of IPv4s
-    and returns a list contain all addresses to check.
-    """
-    # Check if range or single ip
-    if "/" in address:
-        address.split("/")
+    Takes an ipv4 address range + port and tries 
+    establishing a connection for every address.
+    Prints successes to stdout.
 
-def handle_port(port: str) -> bool:
+    returns a list of connectable addresses.
     """
-    Checks if a given tcp port is valid.
-    """
-    pass
+    try:
+        ipaddress.IPv4Network(address)
+    except (ipaddress.AddressValueError, ipaddress.NetmaskValueError, ValueError):
+        print("[!] - Invalid IPv4 or Netmask")
+        return []
+    
+    online: list = []
 
-def check_connection(address: str) -> bool:
+    for ip in ipaddress.IPv4Network(address).hosts():
+        if check_connection(str(ip), port):
+            online.append(ip)
+            print("[+] - Found: ", str(ip))
+
+    return online
+
+def check_connection(address: str, port: int) -> bool:
     """
+    Takes an ipv4 address + port and tries 
+    establishing a connection.
+
+    returns True if a connection is possible and False otherwise.
     """
+
+    # Check if port and ip are valid
+    try:
+        ipaddress.IPv4Address(address)
+    except ipaddress.AddressValueError:
+        print("[!] - Invalid IPv4")
+        return False
+
+    if port < 0 or port > 65535:
+        print("[!] - Invalid Port")
+        return False
+
+
+    sock: socket.socket = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+    sock.settimeout(0.1)    # assumes no connection is possible after 100ms
+    try:
+        sock.connect((address, port))
+        sock.close()
+        return True
+    except (ConnectionError, TimeoutError):
+        return False
